1e5c03eeca846066af0f4a3ed9389917c5c53270
joser021 <joser.dev021@gmail.com>
2025-06-04 13:11:51 -0300

    styles ans js

---
+++ scripts/form.js
@@ -0,0 +1,6 @@
+const today = new Date()
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastmodified");
+
+year.innerHTML = today.getFullYear();
+lastModified.innerHTML = document.lastModified;
